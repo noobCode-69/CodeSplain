347e5bcb51e01beaea2c4f6edf98f18e6e182c88
so_hell <armanyppph@gmail.com>
2023-08-03 21:40:16 +0530

    added test for link

---
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import RepositoriesListItem from "./RepositoriesListItem";
+
+// jest.mock("../tree/FileIcon", () => {
+//   return () => {
+//     return "File Icon Component";
+//   };
+// });
+
+function renderComponent() {
+  const repository = {
+    full_name: "facebook/react",
+    language: "Javascript",
+    description: "A js library",
+    owner: "facebook",
+    name: "react",
+    html_url: "https://github.com/facebook/react",
+  };
+
+  render(
+    <MemoryRouter>
+      <RepositoriesListItem repository={repository} />
+    </MemoryRouter>
+  );
+
+  return {
+    repository,
+  };
+}
+
+test("show a link to the github homepage for this repository", async () => {
+  const { repository } = renderComponent();
+  await screen.findByRole("img", {
+    name: /Javascript/,
+  });
+  const link = screen.getByRole("link", {
+    name: /github repository/i,
+  });
+  expect(link).toHaveAttribute("href", repository.html_url);
+});
